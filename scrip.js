8c1eacfd231a0eef1e200aa04e1252807880e413
SpinWheel0 <130262351+SpinWheel0@users.noreply.github.com>
2023-04-09 03:00:02 +0000

    uhg

---
+++ scrip.js
@@ -0,0 +1,14 @@
+window.onload = function() { // onload is called when the page is fully loaded
+    let wheel = document.querySelector(".wheel"); // select the wheel element
+    let spinBtn = document.querySelector(".spinBtn"); // select the spin button element
+    let value = Math.ceil(Math.random() * 3600); // generate a random value between 0 and 3600
+    spinBtn.onclick = function() { // when the spin button is clicked
+      wheel.style.transform = "rotate(" + value + "deg)"; // rotate the wheel
+      value += Math.ceil(Math.random() * 3600); // generate a random value between 0 and 3600
+      console.log(value); // log the value to the console
+    }; // end spinBtn.onclick
+    int p = img.getRGB(0, 0); // get the color of a pixel at (0, 0)
+    value.toString(); // convert the value to a string
+    value.parseInt(); // convert the value to an integer
+    alert('Hi!'); // alert the user
+  }; // end window.onload
--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-window.onload = function() { // onload is called when the page is fully loaded
-    let wheel = document.querySelector(".wheel"); // select the wheel element
-    let spinBtn = document.querySelector(".spinBtn"); // select the spin button element
-    let value = Math.ceil(Math.random() * 3600); // generate a random value between 0 and 3600
-    spinBtn.onclick = function() { // when the spin button is clicked
-      wheel.style.transform = "rotate(" + value + "deg)"; // rotate the wheel
-      value += Math.ceil(Math.random() * 3600); // generate a random value between 0 and 3600
-      console.log(value); // log the value to the console
-    }; // end spinBtn.onclick
-    value.toString(); // convert the value to a string
-    value.parseInt(); // convert the value to an integer
-    alert('Hi!'); // alert the user
-  }; // end window.onload
+function rotateFunction(){
+    var min = 1024;
+    var max = 9999;
+    var deg = Math.floor(Math.random() * (max - min)) + min;
+    document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+  }
